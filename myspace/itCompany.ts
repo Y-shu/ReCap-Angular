fdf896866fb79aa2bdb183e051e8d2c501b63522
Y-shu <nivik12292@gmail.com>
2018-10-29 17:16:05 +0530

    Modified itCompany - ACCESSD INTERFACE DATA MEMBERS FROM CHILD CLASS

--- myspace/itCompany.ts
+++ myspace/itCompany.ts
@@ -10,43 +10,59 @@ interface Employee{
 }
 
 //class -- which encapsulate data for objects
-class ItCompany{
+class ItCompany {
       cName:string;
        cAddress:string;
-      cEmpDetails:Employee;
+       cEmpDetails:Employee ;
+
       //initilizing the variables of class
-      constructor(cName:string,cAddress:string){
+      constructor(cName:string,cAddress:string,cEmpDetails:Employee){
      //this keyword referes to current instance of class
         this.cName= cName;
         this.cAddress = cAddress;
-        
-        
-        
+       this.cEmpDetails =   cEmpDetails ;
+        //  console.log(cEmpDetails);
+         
       }
       getDetails():void{
-        console.log(`Hey Welcome to ${this.cName} which is in ${this.cAddress}`);
+        // console.log(this.cEmpDetails);
+        console.log(`this is what i want ${this.cEmpDetails.name}`)
+        console.log(`Hey Welcome to ${this.cName} which is in ${this.cAddress}`);        
     }
+    
 }
+
+
+
 
 class EmpDetails extends ItCompany{
     eLocation:string;
     
-    
-      constructor(cName:string,cAddress:string,eLocation:string){
-        super(cName,cAddress);
-          this.eLocation=eLocation;
+
+      constructor(cName:string,cAddress:string,eLocation:string,cEmpDetails:Employee){
+        super(cName,cAddress,cEmpDetails);
+        
           
                     }
-    //   getEmpDetails():void{
-    //       console.log(`This is ${this.name}`);
-   //   }
+      getEmpDetails():void{
+          console.log(`This is ${this.cEmpDetails.name}`);
+     }
   
 }
-//instance creation of object from clsass
-var empDetails = new EmpDetails("Shell","New York","Banglore");
-//accessing attributes and functions 
-empDetails.getDetails();
-// empDetails.getEmpDetails();
+
+let x = new EmpDetails("mayur","address","hyderabad",{name:"hero",
+email:"dfsd",salary:87,pfamount:87,domain:"wfw",isExperienced:true}) ; 
+
+x.getEmpDetails() ;
+
+
+
+
+// //instance creation of object from clsass
+// var empDetails = new EmpDetails("Shell","New York","Banglore");
+// //accessing attributes and functions 
+// empDetails.getDetails();
+// // empDetails.getEmpDetails();
 
 
 
