1343bee138281cc3bbe595a7b78b4452b94289b8
Y-shu <nivik12292@gmail.com>
2018-10-30 13:11:27 +0530

    CHILD CLASS NEED NOT BE A CLASS IT CAN BE AN INTERFACE ALSO...

---
+++ myspace/class-interface.ts
@@ -0,0 +1,14 @@
+class Color{
+    color1:string;
+    color2:string;
+}
+interface Paint extends Color{
+    color3:string;
+}
+// let painting = new Paint();
+let painting:Paint={
+color1:"Blue",
+color2:"Black",
+color3:"Red"
+}
+console.log(painting);
